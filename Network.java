502ff21e02020428ec0e38f204bb6d355c879f3d
Omer H <omer.h1910@gmail.com>
2026-01-06 17:49:46 +0200

    finished network class

--- Network.java
+++ Network.java
@@ -87,17 +87,25 @@ public class Network {
     /** Computes and returns the name of the most popular user in this network: 
      *  The user who appears the most in the follow lists of all the users. */
     public String mostPopularUser() {
-        //// Replace the following statement with your code
-        return null;
+        int max = -1;
+        String mostPopularUser = null;
+        
+        for(int i = 0; i < userCount; i++){
+            if(followeeCount(users[i].getName()) > max){
+                mostPopularUser = users[i].getName();
+            }
+        }
+        return mostPopularUser;
     }
 
     /** Returns the number of times that the given name appears in the follows lists of all
      *  the users in this network. Note: A name can appear 0 or 1 times in each list. */
     private int followeeCount(String name) {
-        
-        //// Replace the following statement with your code
-        
-        return 0;
+        int count = 0;
+        for(int i = 0; i < userCount; i++){
+            if(users[i].follows(name)) count++;
+        }
+        return count;
     }
 
     // Returns a textual description of all the users in this network, and who they follow.
